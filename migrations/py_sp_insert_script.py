c4e4c0fe0f59e084527b9023c7e19d051c343e05
Gokul <gokul12cool@gmail.com>
2022-11-09 12:23:46 +0400

    Snowpark DCM

--- migrations/py_sp_insert_script.py
+++ migrations/py_sp_insert_script.py
@@ -1,7 +1,8 @@
-#Importing required libraries
-
+## Define the function for the Stored Procedure
+import snowflake.snowpark
 from snowflake.snowpark import Session
-from snowflake.snowpark.types import StructField()
+from snowflake.snowpark.functions import sproc
+from snowflake.snowpark.types import StringType
 
 #Connecting to snowflake
 connection_parameters = {
@@ -10,67 +11,65 @@ connection_parameters = {
 "password": "March20311",
 "role": "ACCOUNTADMIN",
 "warehouse": "SNOWFLAKE_WAREHOUSE",
-"database": "OIL_AND_GAS",
+"database": "SCM_PROD",
 "schema": "CONFORMED"
  }
 
 session = Session.builder.configs(connection_parameters).create()
 
-## Define the function for the Stored Procedure
+session.add_packages('snowflake-snowpark-python')
 
-def insert_into_table(snowpark_session: session):
-    snowpark_session.sql('''
-                        create or replace TABLE OIL_AND_GAS.CONFORMED.CUSTOMER_TABLE 
+def insert_into_table(session: snowflake.snowpark.Session):
+
+
+    session.sql('''
+                create or replace TABLE SCM_PROD.CONFORMED.CUSTOMER_TABLE 
+                (
+                    CUSTOMER_ID     NUMBER(38,0),
+                    DATE            VARCHAR(16777216),
+                    SALUTATION      VARCHAR(5),
+                    FIRST_NAME      VARCHAR(200),
+                    LAST_NAME       VARCHAR(200),
+                    DATE_INSERT     TIMESTAMP_NTZ(9)
+                ); 
+                ''').collect()
+  
+
+    session.sql('''
+                INSERT INTO SCM_PROD.CONFORMED.CUSTOMER_TABLE
                         (
-                            CUSTOMER_ID     NUMBER(38,0),
-                            DATE            VARCHAR(16777216),
-                            SALUTATION      VARCHAR(5),
-                            FIRST_NAME      VARCHAR(200),
-                            LAST_NAME       VARCHAR(200),
-                            DATE_INSERT     TIMESTAMP_NTZ(9)
-                        ); 
-                        ''').collect()
+                            CUSTOMER_ID,         
+                            DATE,                
+                            SALUTATION,          
+                            FIRST_NAME,          
+                            LAST_NAME,
+                            DATE_INSERT
+                            )
+                SELECT C_CUSTOMER_SK
+                            , TO_CHAR(D_DATE, 'YYYYMMDD')
+                            , C_SALUTATION
+                            , C_FIRST_NAME
+                            , C_LAST_NAME
+                            , CURRENT_TIMESTAMP()
+                FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER C
+                JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.DATE_DIM D 
+                ON D_DATE_SK = C_FIRST_SHIPTO_DATE_SK
+                LIMIT 100
+                ''').collect()
   
-
-    snowpark_session.sql('''
-                        INSERT INTO OIL_AND_GAS.CONFORMED.CUSTOMER_TABLE
-                                (
-                                    CUSTOMER_ID,         
-                                    DATE,                
-                                    SALUTATION,          
-                                    FIRST_NAME,          
-                                    LAST_NAME,
-                                    DATE_INSERT
-                                 )
-                        SELECT C_CUSTOMER_SK
-                                    , TO_CHAR(D_DATE, 'YYYYMMDD')
-                                    , C_SALUTATION
-                                    , C_FIRST_NAME
-                                    , C_LAST_NAME
-                                    , CURRENT_TIMESTAMP()
-                        FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.CUSTOMER C
-                        JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.DATE_DIM D 
-                        ON D_DATE_SK = C_FIRST_SHIPTO_DATE_SK
-                        LIMIT 100
-                        ''').collect()
-  
-    ## Execute a star select query into a Snowflake dataframe
-    results = snowpark_session.sql('SELECT * FROM OIL_AND_GAS.CONFORMED.CUSTOMER_TABLE').collect()
-
-    return results
+    return 'SUCCESS!'
 
 ## Register Stored Produre in Snowflake
 ## Add packages and data types
 
-session.add_packages('snowflake-snowpark-python')
 
-### Upload Stored Produre to Snowflake
+## Upload Stored Produre to Snowflake
 session.sproc.register(
     func = insert_into_table
-  , return_type = StructField()
+  , return_type = StringType()
   , input_types = []
   , is_permanent = True
-  , name = 'SNOWPARK_insert_into_table'
+  , name = 'SCM_PROD.CONFORMED.SNOWPARK_insert_into_table'
   , replace = True
   , stage_location = '@MY_STAGE'
 )
